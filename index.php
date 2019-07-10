b7e7dd6edc2a4a98f2d490ef4be7922addfa5120
rodrigoaives <rodrigoaives@outlook.com>
2019-07-09 22:18:15 -0300

    Adicionado as classes da Hcode

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+	
+	//Namespace
 	$sql = new Hcode\DB\Sql();
 	
 	$results = $sql->select("SELECT * FROM tb_users");
