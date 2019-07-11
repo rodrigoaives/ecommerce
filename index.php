d4bb8da086418d6d9221aea77fadd02e734ee0ad
rodrigoaives <rodrigoaives@outlook.com>
2019-07-11 14:16:45 -0300

    Ajustado tela admin - Caminhos estavam incorretos (na Classe e no html em si)

--- index.php
+++ index.php
@@ -1,38 +1,32 @@
 <?php 
-
+ 
 require_once("vendor/autoload.php");
-
-// Namespaces (para dizer a origem das Classes)
+ 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
-
-// O slim serve para criar uma nova aplicac ao usando o Slim
+ 
 $app = new Slim();
-
+ 
 $app->config('debug', true);
-
+ 
 $app->get('/', function() {
-
-	// Cria a nova pagina
-	$page = new Page(); 
-
-	// Define a pagina principal
-	$page->setTpl("index");
-
+    
+	$page = new Page();
+ 
+	$page ->setTpl("index");
+ 
 });
-
-$app->get('/admin', function() {
-
-	// Cria a nova pagina
-	$page = new PageAdmin(); 
-
-	// Define a pagina principal
-	$page->setTpl("index");
-
+ 
+ 
+$app->get('/admin(/)', function() {
+    
+	$page = new PageAdmin();
+ 
+	$page ->setTpl("index");
+ 
 });
-
-// Roda a aplicacao
+ 
 $app->run();
-
- ?>
+ 
+?>
--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -1,16 +1,12 @@
 <?php
+ 
+ namespace Hcode;
+ use Rain\Tpl;
+  
+ class PageAdmin extends Page{
+     public function __construct($opts = array(), $tpl_dir = "/views/admin/") {
+         parent::__construct($opts, $tpl_dir);
+     }
+ }
 
-namespace Hcode; 
-
-class PageAdmin extends Page { 
-
-    public function __contruct($opts = array(), $tpl_dir = "../views/admin/") {
-
-        // Pega a configuracao do construct do Page.php
-        parent::__construct($opts, $tpl_dir);
-
-    }
-
-}
-
-?>
+ ?>
