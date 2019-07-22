4233e315733135c4e3bab1cbc40c55171cad2319
rodrigoaives <rodrigoaives@outlook.com>
2019-07-22 10:31:51 -0300

    Site - Categorias

---
+++ views-cache/categories-menu.2506bbf2042bfe3cd7ba04929e7d76d6.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/10">Android</a></li><li><a href="/categories/6">Apple</a></li><li><a href="/categories/5">Google</a></li><li><a href="/categories/7">Motorola</a></li><li><a href="/categories/8">Samsung</a></li>
---
+++ views-cache/category.2506bbf2042bfe3cd7ba04929e7d76d6.rtpl.php
@@ -0,0 +1,49 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="product-big-title-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="product-bit-title text-center">
+                    <h2><?php echo htmlspecialchars( $category["descategory"], ENT_COMPAT, 'UTF-8', FALSE ); ?></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="single-product-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
+
+            <?php } ?>
+
+        </div>
+        
+        <div class="row">
+            <div class="col-md-12">
+                <div class="product-pagination text-center">
+                    <nav>
+                        <ul class="pagination">
+                        <li>
+                            <a href="#" aria-label="Previous">
+                            <span aria-hidden="true">«</span>
+                            </a>
+                        </li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                            <span aria-hidden="true">»</span>
+                            </a>
+                        </li>
+                        </ul>
+                    </nav>                        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
