7855d1e2db66b4f9594ddd08615f4e3ea1448721
rodrigoaives <rodrigoaives@outlook.com>
2019-07-29 09:25:37 -0300

    Alterar Senha - Site

--- site.php
+++ site.php
@@ -609,6 +609,83 @@ $app->get("/profile/orders/:idorder", function($idorder){
 
 });
 
+$app->get("/profile/change-password", function() {
+
+	User::checkLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password", [
+		'changePassError'=>User::getMsgError(), 
+		'changePassSuccess'=>User::getSuccess()
+	]);
+
+});
+
+$app->post("/profile/change-password", function() {
+
+	User::checkLogin(false);
+
+
+	if ((!isset($_POST['current_pass'])) || ($_POST['current_pass'] === '')) { 
+
+		User::setMsgError("Digite a senha atual.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if ((!isset($_POST['new_pass'])) || ($_POST['new_pass'] === '')) { 
+
+		User::setMsgError("Digite a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if ((!isset($_POST['new_pass_confirm'])) || ($_POST['new_pass_confirm'] === '')) { 
+
+		User::setMsgError("Confirme a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if ($_POST['current_pass'] === $_POST['new_pass']) {
+
+		User::setMsgError("Sua nova senha deve ser diferente da senha atual.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if ($_POST['new_pass'] != $_POST['new_pass_confirm']) {
+
+		User::setMsgError("Os campos: 'nova senha' e 'confirme a nova senha' não coincidem.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	$user = User::getFromSession();
+
+	if(!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+		User::setMsgError("A senha atual informada é inválida.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	User::setSuccess("Senha alterada com sucesso.");
+
+	header("Location: /profile/change-password");
+	exit;
+});
 
 
 ?>
