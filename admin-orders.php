cc5a00ef73b568803f5db80b6cfaf98e1cc3430b
rodrigoaives <rodrigoaives@outlook.com>
2019-07-29 12:09:26 -0300

    Alterar Status - Admin

---
+++ admin-orders.php
@@ -0,0 +1,104 @@
+<?php
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
+
+$app->get("/admin/orders/:idorder/status", function($idorder) {
+
+    User::verifyLogin();
+
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("order-status", [
+        'order'=>$order->getValues(),
+        'status'=>OrderStatus::listAll(),
+        'msgError'=>Order::getMsgError(),
+        'msgSuccess'=>Order::getMsgSuccess()
+    ]);
+
+});
+
+$app->post("/admin/orders/:idorder/status", function($idorder) {
+
+    User::verifyLogin();
+
+    if (!isset($_POST['idstatus']) || !(int)$_POST['idstatus'] > 0) {
+        Order::setMsgError("Informe o status atual.");
+        header("Location: /admin/orders/" . $idorder . "/status");
+        exit;
+    }
+    
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $order->setidstatus((int)$_POST['idstatus']);
+
+    $order->save();
+
+    Order::setMsgSuccess("Status atualizado.");
+
+    header("Location: /admin/orders/" . $idorder . "/status");
+    exit;
+
+});
+
+$app->get("/admin/orders/:idorder/delete", function($idorder) {
+
+    User::verifyLogin();
+
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $order->delete();
+
+    header("Location: /admin/orders");
+    exit;
+
+});
+
+$app->get("/admin/orders/:idorder", function($idorder) {
+
+    User::verifyLogin();
+
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $cart = $order->getCart();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("order", [
+        'order'=>$order->getValues(),
+        'cart'=>$cart->geValues(),
+        'products'=>$cart->getProducts()
+    ]);
+
+    var_dump($order);
+    exit;
+
+});
+
+$app->get("/admin/orders", function() {
+
+    User::verifyLogin();
+    
+    $page = new PageAdmin();
+
+    $page->setTpl("orders", [
+        'orders'=>Order::listAll()
+    ]);
+
+});
+
+
+
+?>
--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -4,8 +4,12 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Model\Cart;
 
 class Order extends Model {
+
+    const SUCCESS = "Order-Success";
+    const ERROR = "Order-Error";
 
     public function save() {
 
@@ -23,6 +27,16 @@ class Order extends Model {
         if (count($results) > 0) {
             $this->setData($results[0]);
         }
+
+    }
+
+    public function delete() {
+
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_orders WHERE idorder = :idorder", [
+            ':idorder'=>$this->getidorder()
+        ]);
 
     }
 
@@ -47,6 +61,88 @@ class Order extends Model {
         }
 
     }
+
+    public static function listAll() 
+    {
+
+        $sql = new Sql();
+
+        return 
+        $sql->select("SELECT *
+        FROM tb_orders a 
+        INNER JOIN tb_ordersstatus b USING(idstatus)
+        INNER JOIN tb_carts c USING (idcart)
+        INNER JOIN tb_users d ON d.iduser = a.iduser
+        INNER JOIN tb_addresses e USING(idaddress)
+        INNER JOIN tb_persons f ON f.idperson = d.idperson 
+        ORDER BY a.dtregister DESC
+        ");
+
+    }
+
+    public function getCart():Cart // Tipo de Retorno.
+    {
+
+        $cart = new Cart();
+
+        $cart->get((int)$this->getidcart());
+
+        return $cart;
+
+    }
+
+
+    public static function setMsgError($msg)
+    {
+
+        $_SESSION[Order::ERROR] = $msg;
+
+    }   
+
+    public static function getMsgError()
+    {
+
+        $msg = (isset($_SESSION[Order::ERROR])) ? $_SESSION[Order::ERROR] : "";      
+
+        Order::clearMsgError();
+
+        return $msg; 
+
+    }
+
+    public static function clearMsgError()
+    {
+
+        $_SESSION[Order::ERROR] = NULL;
+
+    }
+
+
+    public static function setMsgSuccess($msg)
+    {
+
+        $_SESSION[Order::SUCCESS] = $msg;
+
+    }   
+
+    public static function getMsgSuccess()
+    {
+
+        $msg = (isset($_SESSION[Order::SUCCESS])) ? $_SESSION[Order::SUCCESS] : "";      
+
+        Order::clearMsgSuccess();
+
+        return $msg; 
+
+    }
+
+    public static function clearMsgSuccess()
+    {
+
+        $_SESSION[Order::SUCCESS] = NULL;
+
+    }
+
 
 }
 
